d1872e763630540879543f74380f21e40bc636d6
Saeed Gholami <saeed.ghollami@gmail.com>
2022-05-09 21:46:40 +0430

    Learn about innerHTML

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Running script.js");
+let firstName = "John";
+let age = 34;
+let message = "My name is " + firstName + "and I am " + age;
+document.getElementById("msg").innerHTML = message;
